973175f37f40707764e74c0ac7e6bae0190a674b
julia <yasminjullia@live.com>
2019-10-29 16:12:16 -0300

    Listview and add metod implements

--- app/src/main/java/com/example/tasklist/MainActivity.java
+++ app/src/main/java/com/example/tasklist/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.tasklist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    final List<String> lix = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +27,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final ListView listView = findViewById(R.id.listView);
+        final TextAdapter adpte = new TextAdapter();
+        adpte.setData(lix);
+        listView.setAdapter(adpte);
+
+        final Button newTaskBtn = findViewById(R.id.newTaskbtn);
+        newTaskBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                final EditText taskInput = new EditText(MainActivity.this);
+                taskInput.setSingleLine();
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Adicionar nova tarefa")
+                        .setMessage("Qual é a tarefa?")
+                        .setView(taskInput)
+                        .setPositiveButton("Adicionar tarefa", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                lix.add(taskInput.getText().toString());
+                                adpte.setData(lix);
+                            }
+                        })
+                        .setNegativeButton("Cancelar", null)
+                        .create();
+                dialog.show();
+            }
+        });
     }
 
     class TextAdapter extends BaseAdapter{
+
+        List<String> list = new ArrayList<>();
+        void setData(List<String>mList){
+            list.clear();
+            list.addAll(mList);
+            notifyDataSetChanged();
+        }
 
         @Override
         public int getCount() {
@@ -39,7 +82,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = (LayoutInflater)MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+           final LayoutInflater inflater = (LayoutInflater)
+                    MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final View rowView = inflater.inflate(R.layout.item, parent, false);
+            final TextView textView = rowView.findViewById(R.id.task);
+            TextView.setText(list.get(position));
+            return rowView;
         }
     }
 }
